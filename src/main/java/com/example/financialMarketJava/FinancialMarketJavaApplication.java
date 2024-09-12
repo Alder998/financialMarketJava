841a91806b11bf21c7648352169ae7f4134ee638
Alder998 <davide.ontano@yahoo.com>
2024-09-12 21:56:02 +0200

    First Commit (Git)

---
+++ src/main/java/com/example/financialMarketJava/FinancialMarketJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.financialMarketJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialMarketJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialMarketJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/financialMarketJava/FinancialMarketJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financialMarketJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialMarketJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
